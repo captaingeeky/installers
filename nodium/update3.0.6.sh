ac8371c06ed3de3ad46e719108047aac10830352
zaxa_crypto <chris@zaxa.ca>
2018-05-11 20:09:01 -0400

    Update update3.0.6.sh

--- nodium/update3.0.6.sh
+++ nodium/update3.0.6.sh
@@ -181,16 +181,18 @@ function start_wallet()
     $DAEMON_START
     echo
     echo -e "${BLUE}Congratulations, you've updated your masternode!${NC}"
-    echo
-    echo -e "${RED}---===>>> TROUBLESHOOTING TIPS <<<===---${NC}"
-    echo -e "${BLUE}If there are any issues with your masternode going to MISSING${NC}"
-    echo -e "${BLUE}in the Windows/Mac wallet, just wait 20 minutes then${NC}"
+    echo -e "${YELLOW}On your Windows/Mac wallet:${NC}"
+    echo -e "${BLUE}Please go to your Masternodes tab, click on your masternode and press on ${YELLOW}Start Alias${NC}"
+  
+  echo
+    echo -e "${RED}---===>>> NEXT STEPS <<<===---${NC}"
+    echo -e "${BLUE}Wait 20-30 minutes for the masternode to announce itself then:${NC}"
     echo -e "${BLUE}type this in the VPN:${NC}"
     echo -e "${GREEN}~/nodium/src/nodium-cli stop${NC}"
     echo -e "${GREEN}~/nodium/src/nodiumd -daemon${NC}"
     echo -e "${BLUE}(or simply reboot your server)${NC}"
-    echo -e "${BLUE}Once restarted, on your Windows/Mac wallet:${NC}"
-    echo -e "${BLUE}go to your Masternodes tab, click on your masternode and press on ${YELLOW}Start Alias${NC}"
+    echo -e "${BLUE}Your wallet timer on your Windows/Mac wallet will start counting up shortly after${NC}"
+    echo -e "${BLUE}and rewards will resume after maturity.${NC}"
   else
     RETVAL=$?
     echo -e "${RED}Binary not found! Please scroll up to see errors above : $RETVAL ${NC}"
