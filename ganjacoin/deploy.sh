24d33ab739ccba150e2c70eed5c8cb0bac4151d2
zaxa_crypto <chris@zaxa.ca>
2018-07-26 17:24:51 -0400

    Update deploy.sh

--- ganjacoin/deploy.sh
+++ ganjacoin/deploy.sh
@@ -1,11 +1,10 @@
-
 #!/bin/bash
 
 VERSION="1.0.52"
 PROJECT="GanjaCoin"
 PROJECT_FOLDER="$HOME/ganja"
 BINARY="ganjacoind"
-  
+
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
@@ -27,7 +26,7 @@ function checks()
   if [ -f /root/.Ganjaproject2 ]; then
     IS_INSTALLED=true
     echo -e "${YELLOW}$PROJECT Previously installed! ${NC}"
-      
+
     read -e -p "$(echo -e ${YELLOW}Delete the current version and continue? [Y/N] ${NC})" CHOICE
     if [[ ("$CHOICE" == "n" || "$CHOICE" == "N") ]]; then
       echo
@@ -55,7 +54,7 @@ function check_existing()
 
   #Get number of existing MRJA masternode directories
   DIR_COUNT=$(ls -la /root/ | grep "\.Ganjaproject" | grep -c '^')
-  
+
   #Check if there are more IPs than existing nodes
   if [[ $DIR_COUNT -ge $IP_NUM ]]; then
     echo -e "${RED}Not enough available IP addresses to run another node! Please add other IPs to this VPS first.${NC}"
@@ -71,12 +70,12 @@ function check_existing()
   if [[ ("$CHOICE" == "n" || "$CHOICE" == "N") ]]; then
     exit 1;
   fi
-  
+
 }
 
 function set_environment()
 {
-  DATADIR="$HOME/.Ganjaproject2$DIR_NUM"
+  DATADIR="$HOME/.Ganjaproject2"
 
   TMP_FOLDER=$(mktemp -d)
   RPC_USER="$PROJECT-Admin"
@@ -92,33 +91,33 @@ function set_environment()
 function show_header()
 {
 clear
-echo -e "${GREEN}                      ,(%%%%%%%%%%%%%(,                    "
-echo -e "                 .#%%/.        /.     ./#%#,               "
-echo -e "              /%%*             %,          /%%/            "
-echo -e "           .#%,               *%,             /%#.         "
-echo -e "         ,%#.                /%%.,              .#%,       "
-echo -e "        ##.                 *%%%*.,               ,%#      "
-echo -e "      *%*                  /#%%%,   .               (%*    "
-echo -e "     /%.                   *%%%% .,,                 ,%/   "
-echo -e "    /%                    .%%%%%....,                 ,%/  "
-echo -e "   /%  /.                 #%%%%%..,*.                  ,%/ "
-echo -e "  .%*   (##/*             (%%%%%.. ../                  /%."
-echo -e "  (#     *%##%###,.       /%%%%%  .,.              ..    %("
-echo -e "  %/      /%###%#%#%#     (%%%%%. , .       . . ..       #%"
-echo -e " .%,        ,#%%%##%%%%%#. #%%%%..,.   *  . .,.          /%"
-echo -e "  %*         .(%%%%%%%%%%%/(%%##(/*,. ..  ..  ,          (%"
-echo -e "  %(           .(%%%%%%%%%#.  .**.  / . ,.. .            ##"
-echo -e "  /%             .#%%%%%%#  (%%% . (.. ,   ..           ,%/"
-echo -e "   %(              .(%%%%(  %%%#.....(..  ..            #% "
-echo -e "   .%*          ./(%##%%%%. .%%%*,(. *...(, ,          /%. "
-echo -e "    .%*   ,/(###(##%%%%%%%%%/.    .//,,*.. .   . .    (%.  "
-echo -e "     .%(     *#####%%%%%%%%%%%%( .... *,  .. ..      #%.   "
-echo -e "       ##.         ..   /%%%%%//  *. ,/  ...       ,%#     "
-echo -e "        *%(           .%%%%%(  *    /..*,        .#%,      "
-echo -e "          /%#        .%%%%/    .     ....,     .#%/        "
-echo -e "               *%%(,                     ,#%%*             "
-echo -e "                   /%%%(*,        .,/#%%%/                 "
-echo -e "                        ,(%%%%%%%%%(,                      ${NC}"
+echo -e "${YELLOW}                      ,(%%%%%%%%%%%%%(,                    "
+echo -e "${YELLOW}                 .#%%/.${GREEN}        /.     ${YELLOW}./#%#,               "
+echo -e "${YELLOW}              /%%*${GREEN}             %,          ${YELLOW}/%%/            "
+echo -e "${YELLOW}           .#%,${GREEN}               *%,             ${YELLOW}/%#.         "
+echo -e "${YELLOW}         ,%#.${GREEN}                /%%.,              ${YELLOW}.#%,       "
+echo -e "${YELLOW}        ##.${GREEN}                 *%%%*.,               ${YELLOW},%#      "
+echo -e "${YELLOW}      *%*${GREEN}                  /#%%%,   .               ${YELLOW}(%*    "
+echo -e "${YELLOW}     /%.${GREEN}                   *%%%% .,,                 ${YELLOW},%/   "
+echo -e "${YELLOW}    /%${GREEN}                    .%%%%%....,                 ${YELLOW},%/  "
+echo -e "${YELLOW}   /%${GREEN}  /.                 #%%%%%..,*.                  ${YELLOW},%/ "
+echo -e "${YELLOW}  .%*${GREEN}   (##/*             (%%%%%.. ../                  ${YELLOW}/%."
+echo -e "${YELLOW}  (#${GREEN}     *%##%###,.       /%%%%%  .,.              ..    ${YELLOW}%("
+echo -e "${YELLOW}  %/${GREEN}      /%###%#%#%#     (%%%%%. , .       . . ..       ${YELLOW}#%"
+echo -e "${YELLOW} .%,${GREEN}        ,#%%%##%%%%%#. #%%%%..,.   *  . .,.          ${YELLOW}/%"
+echo -e "${YELLOW}  %*${GREEN}         .(%%%%%%%%%%%/(%%##(/*,. ..  ..  ,          ${YELLOW}(%"
+echo -e "${YELLOW}  %(${GREEN}           .(%%%%%%%%%#.  .**.  / . ,.. .            ${YELLOW}##"
+echo -e "${YELLOW}  /% ${GREEN}            .#%%%%%%#  (%%% . (.. ,   ..           ${YELLOW},%/"
+echo -e "${YELLOW}   %( ${GREEN}             .(%%%%(  %%%#.....(..  ..            ${YELLOW}#% "
+echo -e "${YELLOW}   .%* ${GREEN}         ./(%##%%%%. .%%%*,(. *...(, ,          ${YELLOW}/%. "
+echo -e "${YELLOW}    .%* ${GREEN}  ,/(###(##%%%%%%%%%/.    .//,,*.. .   . .    ${YELLOW}(%.  "
+echo -e "${YELLOW}     .%( ${GREEN}    *#####%%%%%%%%%%%%( .... *,  .. ..      ${YELLOW}#%.   "
+echo -e "${YELLOW}       ##. ${GREEN}        ..   /%%%%%//  *. ,/  ...       ${YELLOW},%#     "
+echo -e "${YELLOW}        *%(  ${GREEN}         .%%%%%(  *    /..*,        ${YELLOW}.#%,      "
+echo -e "${YELLOW}          /%#  ${GREEN}      .%%%%/    .     ....,    ${YELLOW} .#%/        "
+echo -e "${YELLOW}               *%%(,                     ,#%%*             "
+echo -e "${YELLOW}                   /%%%(*,        .,/#%%%/                 "
+echo -e "${YELLOW}                        ,(%%%%%%%%%(,                      ${NC}"
 sleep 4
 echo -e "${RED}■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■${NC}"
 echo -e "${YELLOW}$PROJECT Masternode Installer v$VERSION - chris 2018"
@@ -138,21 +137,19 @@ echo -e " ${YELLOW}■${NC} Add Brute-Force protection using fail2ban"
 echo -e " ${YELLOW}■${NC} Update the system firewall to only allow SSH, the masternode ports and outgoing connections"
 echo -e " ${YELLOW}■${NC} Add a schedule entry for the service to restart automatically on power cycles/reboots."
 echo
-echo -e " ${RED}WARNING!!! ${RED}If you are already running one or more $PROJECT Masternode(s) on this machine, make sure they are running before executing this script!!! ${NC}"
-echo -e " ${RED}If you do not, the script will improperly detect running nodes and possibly overwrite existing $PROJECT configurations! ${NC}"
 echo
 }
 
 function create_swap()
 {
   echo
-  echo -e "${BLUE}Creating Swap... (ignore errors, this might not be supported or previously installed.)${NC}"
-  fallocate -l 3G /swapfile
-  chmod 600 /swapfile
-  mkswap /swapfile
-  swapon /swapfile
+  echo -e "${BLUE}Creating Swap... ${NC}"
+  fallocate -l 3G /swapfile > /dev/null 2>&1
+  chmod 600 /swapfile > /dev/null 2>&1
+  mkswap /swapfile > /dev/null 2>&1
+  swapon /swapfile > /dev/null 2>&1
   echo
-  echo -e "/swapfile none swap sw 0 0 \n" >> /etc/fstab
+  echo -e "/swapfile none swap sw 0 0 \n" >> /etc/fstab > /dev/null 2>&1
 }
 
 function install_prerequisites()
@@ -160,12 +157,18 @@ function install_prerequisites()
     echo
     echo -e "${BLUE}Installing Pre-requisites${NC}"
     #pre-reqs for running the daemon file
-    sudo apt update
-    sudo apt install -y pwgen build-essential pkg-config libevent-dev libtool libboost-all-dev libgmp-dev libssl-dev libcurl4-openssl-dev
-    sudo add-apt-repository -y ppa:bitcoin/bitcoin
-    sudo apt update
-    sudo apt install -y libdb4.8-dev libdb4.8++-dev
+    sudo apt update > /dev/null 2>&1
+    echo -ne "${YELLOW} [###               ]\r"
+    sudo apt install -y pwgen build-essential pkg-config libevent-dev libtool libboost-all-dev libgmp-dev libssl-dev libcurl4-openssl-dev libdb5.3++-dev > /dev/null 2>&1
+    echo -ne " [########          ]\r"
+    sudo add-apt-repository -y ppa:bitcoin/bitcoin > /dev/null 2>&1
+    echo -ne " [##############    ] \r"
+    sudo apt update > /dev/null 2>&1
+    sudo apt install -y libdb4.8-dev libdb4.8++-dev > /dev/null 2>&1
+    echo -e " [##################]${NC}"
     #end pre-reqs section
+    echo
+    echo -e "${GREEN}Done.${NC}"
 }
 
 function copy_binaries()
@@ -176,16 +179,31 @@ function copy_binaries()
     echo -e "${BLUE}Copying Binaries...${NC}"
     mkdir $PROJECT_FOLDER > /dev/null 2>&1
     cd $PROJECT_FOLDER > /dev/null 2>&1
-  
+
     echo
     wget https://github.com/zaemliss/installers/raw/master/ganjacoin/ganjacoind > /dev/null 2>&1
-    chmod +x $CLI_BINARY > /dev/null 2>&1
-
+    chmod +x $DAEMON > /dev/null 2>&1
+    echo -e "${GREEN}Done${NC}"
+    echo
   if [ -f $DAEMON ]; then
       mkdir $DATADIR
-      echo -e "${BLUE}Starting daemon ...(30 seconds)${NC}"
-      $DAEMON_START
-      sleep 30
+cat <<EOF > $CONF_FILE
+listen=1
+server=0
+maxconnections=500
+rpcuser=yourusername
+rpcpassword=9mRoJDqk1xa8XvOdL9NZhEdhTWzpnhhuT7
+port=12419
+rpcport=12420
+rpcconnect=127.0.0.1
+rpcallowip=127.0.0.1
+addnode=138.197.44.71
+EOF
+      echo -e "${BLUE}Starting daemon ...(5 seconds)${NC}"
+      $DAEMON &
+      sleep 5
+
+#      sed -i "s/server=1/server=0/" .Ganjaproject2/Ganjaproject.conf
     else
       echo -e "${RED}Binary not found! Please scroll up to see errors above : $RETVAL ${NC}"
       exit 1;
@@ -194,45 +212,70 @@ function copy_binaries()
 
 function prepare_QT()
 {
-  clear
   echo
-  echo -e "${YELLOW}QT Wallet Preparation : (you need your Windows or Mac wallet open for this step!)${NC}"
+  echo -e "${YELLOW} QT Wallet Preparation : (you need your Windows or Mac wallet open for this step!)${NC}"
   echo
   echo -e "${BLUE} Step 1. Create a new wallet receiving address. To do this, simply go to the ${GREEN}Receive ${BLUE}tab on the left and"
   echo -e " click on the ${GREEN}New Address${BLUE} button below. A popup window will ask for a label. Write something in to properly"
   echo -e " identify your masternode such as a name and it's number. For example, if this is your first, you could use: ${NC}"
-  echo -e " ${GREEN}MN01 ${BLUE}as an alias. Write it in the label field and click ok.${NC}"
+  echo -e " ${GREEN}MN01 ${BLUE}as an alias. Write it in the label field and click ok. Right-click on that new address and select COPY${NC}"
   echo
   read -e -p "$(echo -e ${YELLOW} Once this is done, please type in the label you chose here and press enter [case sensitive]: ${NC})" MN_ALIAS
-  
+  echo
+  echo
+  echo -e "${BLUE} Step 2. Send ${YELLOW}EXACTLY 30,000 ${BLUE}coins to the new address you created. To do this, simply"
+  echo -e " click on the ${GREEN}Send${BLUE} tab. In the Pay To field, enter the new masternode address by pasting the one you copied in "
+  echo -e " the previous step, then enter the quantity ${GREEN}30,000${BLUE} and press ${GREEN}SEND${NC}."
+  echo -e " ${GREEN}Enter your wallet password when prompted to confirm the transaction.${NC}"
+  echo
+
+  echo -e "${BLUE} Step 3. Now we need the ${YELLOW}Transaction ID and Output${BLUE} of the transaction you just did. For this, you simply"
+  echo -e " click on ${GREEN}Help${BLUE} at the top of your wallet and click ${GREEN}Debug Window ${BLUE}. On the popup window,"
+  echo -e " click on ${GREEN}Console${BLUE}. In the little bar below, type ${GREEN}masternode outputs${BLUE} and press ENTER."
+  echo -e " You should see something like:${NC}"
+  echo "{"
+  echo "  \"01dc01458060d7a5c937245b5eb72c06ab7c886caf36622338acd15906d45a\" : \"1\""
+  echo "}"
+  echo
+  echo -e "${BLUE} The first part is the ${YELLOW}Transaction ID${BLUE} whereas the second part is the ${YELLOW}Transaction Output.${BLUE}"
+  echo -e " If you can't see anything between the brackets it's because your transaction is still pending. If it doesn't work right away, try"
+  echo -e " again in few minutes."
+  echo -e " Once you have a result with the output command, please enter them here WITHOUT QUOTES:"
+  read -e -p "$(echo -e ${YELLOW} Transaction ID: ${NC})" TX_ID
+  read -e -p "$(echo -e ${YELLOW} TXOutput [0 or 1]: ${NC})" TX_OUTPUT
+  if [ TX_OUTPUT == 0 ]; then
+     echo -e "${RED}ERROR! There must be a TX Output and a TX hash to continue."
+     echo -e "Installation Aborted. ${NC}"
+     exit 1
+  fi
+
 }
 
 function create_conf_file()
 {
   echo
   PASSWORD=$(pwgen -s 64 1)
-  GENKEY=$($CLI masternode genkey)
+  GENKEY=$($DAEMON masternode genkey) > /dev/null 2>&1
   echo
   echo -e "${BLUE}Creating conf file...${NC}"
-  echo -e "${YELLOW}Ignore any errors you see below. (15 seconds)${NC}"
   sleep 15
   echo
   echo -e "${BLUE}Stopping the daemon and writing config (15 seconds)${NC}"
-  $CLI stop > /dev/null 2>&1
-  sleep 16
-  
+  $DAEMON stop > /dev/null 2>&1
+  sleep 10
+
 cat <<EOF > $CONF_FILE
 rpcuser=$RPC_USER
 rpcpassword=$PASSWORD
 rpcallowip=localhost
-$RPC_PORT
+rpcport=$RPC_PORT
 port=12419
 externalip=$NEXT_AVAIL_IP
 server=1
 listen=1
 daemon=1
 logtimestamps=1
-txindex=$TX_INDEX
+txindex=$TX_OUTPUT
 maxconnections=500
 mnconflock=0
 masternode=1
@@ -253,7 +296,7 @@ addnode=207.246.117.154:12419
 addnode=78.159.150.22:12419
 addnode=107.175.32.173:12419
 addnode=66.42.64.200:12419
-addnode=216.172.33.74:12419 
+addnode=216.172.33.74:12419
 EOF
 }
 
@@ -263,7 +306,7 @@ function secure_server()
   echo -e "${BLUE}setting up firewall...${NC}"
   sudo apt-get install -y ufw fail2ban > /dev/null 2>&1
   sudo apt-get update -y > /dev/null 2>&1
-  
+
   #configure ufw firewall
   sudo ufw default allow outgoing > /dev/null 2>&1
   sudo ufw default deny incoming > /dev/null 2>&1
@@ -271,7 +314,7 @@ function secure_server()
   sudo ufw limit ssh/tcp > /dev/null 2>&1
   sudo ufw allow $MN_PORT/tcp > /dev/null 2>&1
   sudo ufw logging on > /dev/null 2>&1
-  echo "y" | sudo ufw enable
+  echo "y" | sudo ufw enable > /dev/null 2>&1
 }
 
 function add_cron()
@@ -285,45 +328,66 @@ function start_wallet()
   echo -e "${BLUE}Re-Starting the wallet...${NC}"
   if [ -f $DAEMON ]; then
     echo
-    echo -e "${BLUE}Now wait for a full synchro (can take 10-15 minutes)${NC}"
-    echo -e "${BLUE}Once Synchronized, go back to your Windows/Mac wallet,${NC}"
-    echo -e "${BLUE}go to your Masternodes tab, click on your masternode and press on ${YELLOW}Start Alias${NC}"
+    echo -e "${YELLOW} Final Wallet Configuration:"
+    echo -e "${BLUE} Please go to your QT Wallet (Windows or Mac wallet) and close it.${NC}"
+    echo -e "${BLUE} Next, go to your data folder and double-click on ${GREEN}Ganjaproject2${BLUE} (see methods below)${NC}"
     echo
-    read -n 1 -s -r -p "Press any key to continue to syncronisation steps"
+    echo -e " In Windows: Click the start button and type %APPDATA% and press ENTER, then double-click on the Ganjaproject2 folder"
+    echo -e " In MacOS: with your file explorer, navigate to /Users/USERNAME/Library/Application Support/Ganjacoin2"
     echo
+    echo -e "${BLUE} Once in that folder, you should see a masternode.conf file if you have other masternodes. If not you may have"
+    echo -e " to create it.${NC}"
+    echo -e "${BLUE} Once in that file, copy and paste the following information on a new line:${NC}"
+    echo
+    echo
+    echo -e "$MN_ALIAS $GENKEY $TX_ID $TX_OUTPUT"
+    echo
+    echo
+    echo -e "${BLUE} Next, save the file and close it. Now restart your QT (Windows or Mac) wallet${YELLOW}"
+    read -n 1 -s -r -p " Press any key once this is all done to proceed to the next step."
+    echo -e "${NC}"
+    echo
+
     $DAEMON_START > /dev/null 2>&1
-    echo -e "${BLUE}Starting Synchronization...${NC}"
+    echo -e "${BLUE} Starting Blockchain Synchronization...${NC}"
     sleep 10
-    
-    BLOCKS=$($DAEMON getblockcount)
+
+    BLOCKS=$(curl -s http://explorer.ganjacoinpro.com/api/getblockcount)
     CURBLOCK=$($DAEMON  getinfo | grep blocks | awk {'print $3'} | tr -d ',')
-        
-    while [ $BLOCKS -ne $CURBLOCK ]; do
-      BLOCKS=$($DAEMON getblockcount)
+
+    while [ $CURBLOCK -lt ${BLOCKS-200} ]; do
       CURBLOCK=$($DAEMON  getinfo | grep blocks | awk {'print $3'} | tr -d ',')
-      echo -e "${BLUE}syncing${YELLOW} $CURBLOCK ${BLUE}out of${YELLOW} $BLOCKS ...${NC}"
-      sleep 10
+      echo -ne "${BLUE} syncing${YELLOW} $CURBLOCK ${BLUE}out of${YELLOW} $BLOCKS ${BLUE}...${NC}      \r"
+      sleep 2
     done
-    
-    MNSTATUS=$($DAEMON masternode status | grep status | awk {'print $3'} | tr -d ',')
-    
-    echo -e "${YELLOW}Please right click on your new node in your QT wallet and Start Alias.${NC}"
-    echo -e "${YELLOW}The command prompt will return once your node is started. If the Status goes to Expired in your QT wallet, please start alias again.${NC}"
-    read -n 1 -s -r -p "Press any key to continue"
-    watch -g $CLI masternode status
-    echo -e "${BLUE}Congratulations, you've set up your masternode!${NC}"
     echo
-    echo -e "${RED}Make ${YELLOW}SURE ${RED}you copy this Genkey for your QT wallet (Windows/Mac wallet) ${BLUE}$GENKEY${NC}"
-    echo -e "${BLUE}If you are using Putty, just select the text. It will automatically go to your clipboard.${NC}"
-    echo -e "${BLUE}If you are using SSH, use CTRL-INSERT / CTRL-V${NC}"
-    echo -e "${YELLOW}Typing the key out incorrectly is 99% of all installation issues. ${NC}"
-    echo
-    echo -e "${BLUE}Type ${YELLOW}z <data directory> <command> ${BLUE} to interact with your server(s). ${NC}"
-    echo -e "${BLUE}Ex: ${GREEN}z zixx2 masternode status ${NC}"
-    
+    echo -e "${GREEN} Blockchain synced!${NC}"
+    echo -e "${BLUE} Starting Masternode Synchronization...${NC}"
+    echo -e "${GREEN} When it says ${YELLOW}MASTERNODE Waiting for remote activation!${NC}"
+    echo -e "${GREEN} Go to your Masternode Tab in your QT Wallet, Click on your new Masternode and click on Start."
+
+MNSTAT[0]="MASTERNODE_NOT_PROCESSED"
+MNSTAT[1]="MASTERNODE_IS_CAPABLE"
+MNSTAT[2]="MASTERNODE Waiting for remote activation!"
+MNSTAT[3]="MASTERNODE_STOPPED"
+MNSTAT[4]="MASTERNODE_INPUT_TOO_NEW"
+MNSTAT[6]="MASTERNODE_PORT_NOT_OPEN"
+MNSTAT[7]="MASTERNODE_PORT_OPEN"
+MNSTAT[8]="MASTERNODE_SYNC_IN_PROCESS"
+MNSTAT[9]="MASTERNODE_REMOTELY_ENABLED"
+MNSTATUS=0
+
+    while [ $MNSTATUS -ne 9 ]; do
+      MNSTATUS=$($DAEMON masternode status | grep status | awk {'print $3'} | tr -d ',')
+      echo -ne "${YELLOW} >Masternode Status : ${BLUE}${MNSTAT[MNSTATUS]}${NC} \r"
+      sleep 5
+    done
+
+    echo -e "${BLUE} Congratulations, you've set up your masternode!${NC}"
+
   else
     RETVAL=$?
-    echo -e "${RED}Binary not found! Please scroll up to see errors above : $RETVAL ${NC}"
+    echo -e "${RED} Binary not found! Please scroll up to see errors above : $RETVAL ${NC}"
     exit 1
   fi
 }
@@ -331,11 +395,9 @@ function start_wallet()
 function cleanup()
 {
   cd $HOME
-  
+
   if [ "$IS_CURRENT" = true ] && [ "$IS_INSTALLED" = true ]; then
-    echo -e "${BLUE} Finalizing..."
-  else
-    rm -R db-4.8*
+    echo -e "${BLUE} End of Script."
   fi
 }
 
@@ -343,6 +405,7 @@ function deploy()
 {
   checks
   show_header
+  prepare_QT
   check_existing
   set_environment
   create_swap
@@ -356,3 +419,4 @@ function deploy()
 }
 
 deploy
+
