95c972841f9b796dbb2617712aaa65f75b9e059e
zaxa_crypto <chris@zaxa.ca>
2018-08-21 10:57:56 -0400

    Update install.sh

--- zixx/install.sh
+++ zixx/install.sh
@@ -149,13 +149,13 @@ function install_prerequisites()
     #end libdbcxx section
   
     sudo apt install -y build-essential htop libevent-2.0-5 libzmq5 libboost-system1.58.0 libboost-filesystem1.58.0 libboost-program-options1.58.0 libboost-thread1.58.0 libboost-chrono1.58.0 libminiupnpc10 libevent-pthreads-2.0-5 unzip
-    #sudo wget http://download.oracle.com/berkeley-db/db-4.8.30.zip
-    #sudo unzip db-4.8.30.zip
-    #cd db-4.8.30
-    #cd build_unix/
-    #sudo ../dist/configure --prefix=/usr/ --enable-cxx
-    #sudo make
-    #sudo make install
+    sudo wget http://download.oracle.com/berkeley-db/db-4.8.30.zip
+    sudo unzip db-4.8.30.zip
+    cd db-4.8.30
+    cd build_unix/
+    sudo ../dist/configure --prefix=/usr/ --enable-cxx
+    sudo make
+    sudo make install
   fi
 }
 
