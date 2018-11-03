d6f6679c388744d9d5ad248ef4a04a774ed576c5
zaxa_crypto <chris@zaxa.ca>
2018-11-03 13:12:38 -0400

    Update infopage.sh

--- atheneum/infopage.sh
+++ atheneum/infopage.sh
@@ -4,6 +4,24 @@
 ver="1.0.11"
 getcurrent=$(curl -q https://raw.githubusercontent.com/zaemliss/installers/master/atheneum/versions | jq .infopage | tr -d '"')
 
+red='\033[1;31m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+blue='\033[1;36m'
+clear='\033[0m'
+
+declare -a status
+  status[0]="Initial Masternode Syncronization"
+  status[1]="Syncing Masternode Sporks"
+  status[2]="Syncing Masternode List"
+  status[3]="Syncing Masternode Winners"
+  status[4]="Syncing Budget"
+  status[5]="Masternode Syncronization Timeout"
+  status[10]="Syncing Masternode Budget Proposals"
+  status[11]="Syncing Masternode Finalized Budgets"
+  status[998]="Masternode Sync Failed"
+  status[999]="Masternode Sync Successful"
+
 if ! [[ $ver == $getcurrent ]]; then 
   echo -e "${red} Version outdated! Downloading new version ...${clear}"
   wget https://github.com/zaemliss/installers/raw/master/atheneum/infopage.sh -O infopage.sh
@@ -13,59 +31,38 @@ fi
 
 client=$(find ~/ -name "atheneum-cli" | head -n 1)
 
-red='\033[1;31m'
-green='\033[1;32m'
-yellow='\033[1;33m'
-blue='\033[1;36m'
-clear='\033[0m'
-
-declare -a status
-status[0]="Initial Masternode Syncronization"
-status[1]="Syncing Masternode Sporks"
-status[2]="Syncing Masternode List"
-status[3]="Syncing Masternode Winners"
-status[4]="Syncing Budget"
-status[5]="Masternode Syncronization Timeout"
-status[10]="Syncing Masternode Budget Proposals"
-status[11]="Syncing Masternode Finalized Budgets"
-status[998]="Masternode Sync Failed"
-status[999]="Masternode Sync Successful"
-
 while [ 1 ]; do
+  getinfo=$($client getinfo)
+  mnsync=$($client mnsync status)
+  mnstatus=$($client masternode debug)
 
+  version=$(echo $getinfo | jq .version)
+  protocol=$(echo $getinfo | jq .protocolversion)
+  blocks=$(echo $getinfo | jq .blocks)
+  connections=$(echo $getinfo | jq .connections)
+  supply=$(echo $getinfo | jq .moneysupply)
 
-getinfo=$($client getinfo)
-mnsync=$($client mnsync status)
-mnstatus=$($client masternode debug)
+  blockchainsynced=$(echo $mnsync | jq .IsBlockchainSynced)
+  asset=$(echo $mnsync | jq .RequestedMasternodeAssets)
+  attempt=$(echo $mnsync | jq .RequestedMasternodeAttempt)
 
-version=$(echo $getinfo | jq .version)
-protocol=$(echo $getinfo | jq .protocolversion)
-blocks=$(echo $getinfo | jq .blocks)
-connections=$(echo $getinfo | jq .connections)
-supply=$(echo $getinfo | jq .moneysupply)
+  logresult=$(tail -n 8 ./.atheneum/debug.log)
 
-blockchainsynced=$(echo $mnsync | jq .IsBlockchainSynced)
-asset=$(echo $mnsync | jq .RequestedMasternodeAssets)
-attempt=$(echo $mnsync | jq .RequestedMasternodeAttempt)
-
-logresult=$(tail -n 8 ./.atheneum/debug.log)
-
-
-clear
-echo -e "${blue}Protocol    : ${green}$protocol${clear}"
-echo -e "${blue}Version     : ${green}$version${clear}"
-echo -e "${blue}Connections : ${green}$connections${clear}"
-echo -e "${blue}Supply      : ${green}$supply${clear}"
-echo
-echo -e "${blue}blocks      : ${yellow}$blocks${clear}"
-echo
-echo -e "${blue}Sync Status : ${green}${status[$asset]} ${blue}attempt ${yellow}$attempt ${blue}of ${yellow}8${clear}"
-echo -e "${blue}MN Status   : ${green}$mnstatus${clear}"
-echo
-echo -e "${yellow}==========================================================================="
-echo -e "${blue}$logresult${clear}"
-echo -e "${yellow}===========================================================================${clear}"
-echo -e "${green} Press CTRL-C to exit. Updated every 2 seconds. ${blue} 2018 @bitmonopoly version $ver ${clear}"
-
-sleep 2
+  clear
+  echo -e "${blue}Protocol    : ${green}$protocol${clear}"
+  echo -e "${blue}Version     : ${green}$version${clear}"
+  echo -e "${blue}Connections : ${green}$connections${clear}"
+  echo -e "${blue}Supply      : ${green}$supply${clear}"
+  echo
+  echo -e "${blue}blocks      : ${yellow}$blocks${clear}"
+  echo
+  echo -e "${blue}Sync Status : ${green}${status[$asset]} ${blue}attempt ${yellow}$attempt ${blue}of ${yellow}8${clear}"
+  echo -e "${blue}MN Status   : ${green}$mnstatus${clear}"
+  echo
+  echo -e "${yellow}==========================================================================="
+  echo -e "${blue}$logresult${clear}"
+  echo -e "${yellow}===========================================================================${clear}"
+  echo -e "${green} Press CTRL-C to exit. Updated every 2 seconds. ${blue} 2018 @bitmonopoly version $ver ${clear}"
+  
+  sleep 2
 done
