8333d9eaa47111541d2d3816b577301ee897b829
zaxa_crypto <chris@zaxa.ca>
2018-11-12 18:50:48 -0500

    Update aeminstall.sh

--- atheneum/aeminstall.sh
+++ atheneum/aeminstall.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 #Masternode Installer script by chris, 2018.
 
-VERSION="1.1.4"
+VERSION="1.1.5"
 PROJECT="Atheneum"
 PROJECT_FOLDER="$HOME/Atheneum"
 DAEMON_BINARY="atheneumd"
@@ -243,7 +243,6 @@ EOF
 function create_conf_file()
 {
   echo
-  PASSWORD=$(pwgen -s 64 1)
   GENKEY=$($CLI masternode genkey)
   echo
   echo -e "${BLUE}Creating conf file...${NC}"
