d233835a9ddc19d8599714e1f9146a6734d45a21
zaxa_crypto <chris@zaxa.ca>
2018-11-12 21:07:50 -0500

    Create aem.sh

---
+++ atheneum/aem.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+RED='\033[1;31m'
+BLUE='\033[1;36m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+if [ -z $1 ]; then
+        echo
+        echo -e "${RED}ERROR: you must provide parameters.${NC}"
+        echo -e "Syntax: ${BLUE}aem.sh datadir command [options]${NC}"
+        echo -e "ex.: ${GREEN}aem.sh atheneum2 masternode status${NC}"
+        echo
+        exit 1;
+fi
+
+/root/Atheneum/atheneum-cli -datadir=/root/.$1 -conf=/root/.$1/atheneum.conf $2 $3 $4
