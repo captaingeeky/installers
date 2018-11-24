fd3847a8bc8549c9740518cfd992cea5e0f4d66e
zaxa_crypto <chris@zaxa.ca>
2018-11-24 08:09:26 -0500

    Update nuke.sh

--- IngyInstall/nuke.sh
+++ IngyInstall/nuke.sh
@@ -3,3 +3,4 @@ systemctl stop Ingenuity.service
 sleep 5
 rm -rf backups/ banlist.dat blocks/ budget.dat chainstate/ database/ db.log debug.log fee_estimates.dat .lock mncache.dat mnpayments.dat peers.dat sporks/ zerocoin/
 systemctl start Ingenuity.service
+~/ingyinfo.sh
