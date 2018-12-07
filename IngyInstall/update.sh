bbbb75141f350a6880a82e209f244cceb4d27752
zaxa_crypto <chris@zaxa.ca>
2018-12-07 17:07:40 -0500

    Create update.sh

---
+++ IngyInstall/update.sh
@@ -0,0 +1,5 @@
+cd /usr/local/bin
+rm ing*
+wget https://github.com/IngenuityCoin/Ingenuity/files/2657057/Ingenuity-Daemon-.Ubuntu_16.04.tar.gz
+tar -xf https://github.com/IngenuityCoin/Ingenuity/files/2657057/Ingenuity-Daemon-.Ubuntu_16.04.tar.gz
+cd ~
