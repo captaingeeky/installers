60f321afaea6ffaeb410030b71fea0cd2bf28172
zaxa_crypto <chris@zaxa.ca>
2018-12-07 17:19:09 -0500

    Update update.sh

--- IngyInstall/update.sh
+++ IngyInstall/update.sh
@@ -2,7 +2,7 @@ cd /usr/local/bin
 rm ing*
 wget https://github.com/IngenuityCoin/Ingenuity/files/2657057/Ingenuity-Daemon-.Ubuntu_16.04.tar.gz
 wget https://raw.githubusercontent.com/zaemliss/installers/master/IngyInstall/i.sh && chmod +x i.sh
-tar -xf https://github.com/IngenuityCoin/Ingenuity/files/2657057/Ingenuity-Daemon-.Ubuntu_16.04.tar.gz
+tar -xf Ingenuity-Daemon-.Ubuntu_16.04.tar.gz
 cd ~
 i.sh ingenuity stop
 sleep 5
