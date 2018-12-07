8375c195be379d6f88574f2d28363694ea9a1c03
zaxa_crypto <chris@zaxa.ca>
2018-12-07 17:34:47 -0500

    Create updt2.sh

---
+++ IngyInstall/updt2.sh
@@ -0,0 +1,17 @@
+cd /usr/local/bin
+rm ing*
+wget https://github.com/IngenuityCoin/Ingenuity/files/2657057/Ingenuity-Daemon-.Ubuntu_16.04.tar.gz
+wget https://raw.githubusercontent.com/zaemliss/installers/master/IngyInstall/i.sh -O i.sh && chmod +x i.sh
+tar -xf Ingenuity-Daemon-.Ubuntu_16.04.tar.gz
+cd ~
+i.sh ingenuity stop
+sleep 5
+i.sh ingenuity start
+echo "Daemon updated"
+i.sh ingenuity --version
+
+i.sh ingenuity2 stop
+i.sh ingenuity3 stop
+sleep 3
+i.sh ingenuity2 start
+i.sh ingenuity3 start
