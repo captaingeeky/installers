eabcf0150fef90d6835fbf9b9a40d8517b15f28e
zaxa_crypto <chris@zaxa.ca>
2018-12-10 15:22:16 -0500

    Update nuke.sh
    added addnodes after nuking block data

--- IngyInstall/nuke.sh
+++ IngyInstall/nuke.sh
@@ -3,4 +3,13 @@ systemctl stop Ingenuity.service
 sleep 5
 rm -rf backups/ banlist.dat blocks/ budget.dat chainstate/ database/ db.log debug.log fee_estimates.dat .lock mncache.dat mnpayments.dat peers.dat sporks/ zerocoin/
 systemctl start Ingenuity.service
+sleep 3
+ingenuity-cli addnode 104.248.113.141 add
+ingenuity-cli addnode 104.248.121.23 add
+ingenuity-cli addnode 138.197.217.53 add
+ingenuity-cli addnode 206.189.11.138 add
+ingenuity-cli addnode 128.199.203.87 add
+ingenuity-cli addnode 178.62.77.53 add
+ingenuity-cli addnode 104.248.33.190 add
+ingenuity-cli addnode 178.128.233.157 add
 ~/ingyinfo.sh
