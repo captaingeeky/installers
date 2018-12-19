15d045a3282f7b0a7171f0130ae5573825d02d56
zaxa_crypto <chris@zaxa.ca>
2018-12-19 16:07:10 -0500

    Create updater.sh

---
+++ zixx/updater.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VERSION="1.2.19"
+PROJECT="Zixx"
+PROJECT_FOLDER="$HOME/zixx"
+DAEMON_BINARY="zixxd"
+CLI_BINARY="zixx-cli"
+  
+red='\033[1;31m'
+grn='\033[1;32m'
+yel='\033[1;33m'
+blu='\033[1;36m'
+clear='\033[0m'
+
+printf '\e[48;5;0m'
+clear
+
+client=$(find / -name "zixx-cli" | head -n 1)
+workDir=$(basename "$client")
+
+#Get number of existing Zixx masternode directories
+DIR_COUNT=$(ls -la /root/ | grep "\.zixx" | grep -c '^')
+
+if [[ $DIR_COUNT -lt 1 ]]; then
+  echo -e "${red}No data directories found! Please make sure you have Zixx Masternodes installed on this server."
+  exit 1;
+fi
