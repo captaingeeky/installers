196ed77d0585c14c2c333fccf2d5c492bcd3b7cf
zaxa_crypto <chris@zaxa.ca>
2018-12-19 16:41:00 -0500

    Update updater.sh

--- zixx/updater.sh
+++ zixx/updater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="1.2.19"
+VERSION="1.0.4"
 PROJECT="Zixx"
 PROJECT_FOLDER="$HOME/zixx"
 DAEMON_BINARY="zixxd"
@@ -15,6 +15,8 @@ clear='\033[0m'
 printf '\e[48;5;0m'
 clear
 
+echo -e "${yel}Searching for Zixx binaries and installation directories...${clr}"
+echo
 client=$(find / -name "zixx-cli" | head -n 1)
 workDir=$(basename "$client")
 
@@ -22,9 +24,41 @@ workDir=$(basename "$client")
 DIR_COUNT=$(ls -la /root/ | grep "\.zixx" | grep -c '^')
 
 if [[ $DIR_COUNT -lt 1 ]]; then
-  echo -e "${red}No data directories found! Please make sure you have Zixx Masternodes installed on this server.${clear}"
+  echo -e "${red}No data directories found! Please make sure you have Zixx Masternodes installed on this server.${clr}"
   exit 1;
 fi
-
-echo "${blu}$DIR_COUNT Zixx installations found!${clear}"
+echo -e "${grn}Zixx binaries found in $workDir .${clr}"
+echo -e "${blu}$DIR_COUNT Zixx installations found!${clr}"
 echo
+
+echo -e "${grn}Stopping Daemon using datadir /.zixx"
+$client -datadir=/root/.zixx -conf=/root/.zixx/zixx.conf stop
+
+if [[ $DIR_COUNT -gt 1 ]]; then
+  for i in `seq 2 $DIR_COUNT`; 
+    do 
+      echo -e "${grn}Stopping Daemon using datadir /.zixx$i"
+      $client -datadir=/root/.zixx$i -conf=/root/.zixx$i/zixx.conf stop
+    done
+fi
+echo
+
+echo -e "${blu}Downloading new clients...${clr}"
+echo
+wget https://github.com/zixxcrypto/zixxcore/releases/download/v0.16.5/zixxd -O $workDir/zixxd
+wget https://github.com/zixxcrypto/zixxcore/releases/download/v0.16.5/zixx-cli -O $workDir/zixx-cli
+
+echo -e "${grn}Starting Daemon using datadir /.zixx"
+$workDir/zixxd -datadir=/root/.zixx -conf=/root/.zixx/zixx.conf -daemon
+
+if [[ $DIR_COUNT -gt 1 ]]; then
+  for i in `seq 2 $DIR_COUNT`; 
+    do 
+      echo -e "${grn}Starting Daemon using datadir /.zixx$i"
+      $workDir/zixxd -datadir=/root/.zixx$i -conf=/root/.zixx$i/zixx.conf -daemon
+    done
+fi
+echo
+
+echo -e "${blu}Update complete. Zixx now updated to version below."
+$client -datadir=/root/.zixx$i -conf=/root/.zixx$i/zixx.conf --version
