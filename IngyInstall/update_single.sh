5a341359ed266609656f90c4139e0064fead4bbc
BitMonopoly <chris@zaxa.ca>
2019-02-27 09:42:54 -0500

    Update update_single.sh
    Updated for single MN per vps scenario with more feedback.

--- IngyInstall/update_single.sh
+++ IngyInstall/update_single.sh
@@ -1,14 +1,31 @@
+tarFILE="Ingenuity-Daemon-.Ubuntu_16.04.tar.gz"
+tarURL="https://github.com/IngenuityCoin/Ingenuity/files/2657057/$tarFILE"
+PROJECT="Ingenuity"
+
+red='\033[1;31m'
+grn='\033[1;32m'
+yel='\033[1;33m'
+blu='\033[1;36m'
+clr='\033[0m'
+
+echo -e "${yel}Updating $PROJECT binaries...${clr}"
 cd /usr/local/bin
 rm ing*
-wget https://github.com/IngenuityCoin/Ingenuity/files/2657057/Ingenuity-Daemon-.Ubuntu_16.04.tar.gz
-tar -xf Ingenuity-Daemon-.Ubuntu_16.04.tar.gz
+wget $tarURL
+tar -xf $tarFILE
+rm $tarFILE
+echo
+echo -e "${grn}Stopping Daemon...${clr}"
 cd ~
 systemctl stop Ingenuity.service
 sleep 15
-systemctl start Ingenuity.service
-echo "Daemon updated"
-ingenuity-cli --version
 echo
-echo "**************************************************************"
-echo "** Remember to restart your masternode in your QT wallet!!! **"
-echo "**************************************************************"
+echo -e "${grn}Starting Daemon...${clr}"
+systemctl start Ingenuity.service
+echo -e "${blu}Daemon updated to version below:${yel}"
+ingenuity-cli --version
+echo -e "${clr}"
+echo "${yel}****************************************************************"
+echo "** ${red}Remember to download the new QT wallet (if applies) and to ${yel}**"
+echo "** ${red}restart your masternode in your QT wallet!!!               ${yel}**"
+echo "****************************************************************${clr}"
