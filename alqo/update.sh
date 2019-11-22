80698ebc7666e9002b5c09fe1e9cf2e09c0ca7eb
captaingeeky <35855107+captaingeeky@users.noreply.github.com>
2019-11-22 14:39:30 -0500

    Update update.sh

--- alqo/update.sh
+++ alqo/update.sh
@@ -5,7 +5,7 @@ VERSION="1.0.6"
 PROJECT="Atheneum"
 PROJECT_FOLDER="$HOME/Atheneum"
 DAEMON_BINARY="atheneumd"
-CLI_BINARY="atheneum-cli"
+CLI_BINARY="alqo-cli"
   
 red='\033[1;31m'
 grn='\033[1;32m'
@@ -16,30 +16,30 @@ clr='\033[0m'
 printf '\e[48;5;0m'
 clear
 
-echo -e "${yel}Searching for Atheneum binaries and installation directories...${clr}"
+echo -e "${yel}Searching for Alqo binaries and installation directories...${clr}"
 echo
-client=$(find ~/ -name "atheneum-cli" | head -n 1) > /dev/null 2>&1
+client=$(find ~/ -name "alqo-cli" | head -n 1) > /dev/null 2>&1
 workDir=$(dirname "$client")
 
 #Get number of existing Atheneum masternode directories
-DIR_COUNT=$(ls -la /root/ | grep "\.Atheneum" | grep -c '^')
+DIR_COUNT=$(ls -la /root/ | grep "\.alqo" | grep -c '^')
 
 if [[ $DIR_COUNT -lt 1 ]]; then
-  echo -e "${red}No data directories found! Please make sure you have Atheneum Masternodes installed on this server.${clr}"
+  echo -e "${red}No data directories found! Please make sure you have Alqo Masternodes installed on this server.${clr}"
   exit 1;
 fi
-echo -e "${grn}Atheneum binaries found in $workDir .${clr}"
-echo -e "${blu}$DIR_COUNT Atheneum installations found!${clr}"
+echo -e "${grn}Alqo binaries found in $workDir .${clr}"
+echo -e "${blu}$DIR_COUNT Alqo installations found!${clr}"
 echo
 
-echo -e "${grn}Stopping Daemon using datadir /.Atheneum"
-$client -datadir=/root/.Atheneum -conf=/root/.Atheneum/atheneum.conf stop
+echo -e "${grn}Stopping Daemon using datadir /.alqo"
+$client -datadir=/root/.alqo -conf=/root/.alqo/atheneum.conf stop
 
 if [[ $DIR_COUNT -gt 1 ]]; then
   for i in `seq 2 $DIR_COUNT`; 
     do 
-      echo -e "${grn}Stopping Daemon using datadir .Atheneum$i"
-      $client -datadir=/root/.Atheneum$i -conf=/root/.Atheneum$i/atheneum.conf stop
+      echo -e "${grn}Stopping Daemon using datadir .alqo$i"
+      $client -datadir=/root/.alqo$i -conf=/root/.alqo$i/alqo.conf stop
     done
 fi
 echo
@@ -48,19 +48,19 @@ echo -e "${blu}Downloading new binaries...${clr}"
 sleep 20
 echo
 cd $workDir
-rm atheneumd
-rm atheneum-cli
-wget https://github.com/zaemliss/installers/releases/download/1.0.0/atheneumd > /dev/null 2>&1
-wget https://github.com/zaemliss/installers/releases/download/1.0.0/atheneum-cli > /dev/null 2>&1
-chmod +x $workDir/atheneumd $workDir/atheneum-cli
-echo -e "${grn}Starting Daemon using datadir /.Atheneum"
-$workDir/atheneumd -datadir=/root/.Atheneum -conf=/root/.Atheneum/atheneum.conf -daemon
+rm alqod
+rm alqo-cli
+wget https://github.com/zaemliss/installers/releases/download/1.0.0/atheneumd > /dev/null 2>&1  #need static installer
+wget https://github.com/zaemliss/installers/releases/download/1.0.0/atheneum-cli > /dev/null 2>&1 #need static installer
+chmod +x $workDir/atheneumd $workDir/alqo-cli
+echo -e "${grn}Starting Daemon using datadir /.alqo"
+$workDir/alqod -datadir=/root/.alqo -conf=/root/.alqo/alqo.conf -daemon
 
 if [[ $DIR_COUNT -gt 1 ]]; then
   for i in `seq 2 $DIR_COUNT`; 
     do 
-      echo -e "${grn}Starting Daemon using datadir /.Atheneum$i"
-      $workDir/atheneumd -datadir=/root/.Atheneum$i -conf=/root/.Atheneum$i/atheneum.conf -daemon
+      echo -e "${grn}Starting Daemon using datadir /.alqo$i"
+      $workDir/alqod -datadir=/root/.alqo$i -conf=/root/.alqo$i/alqo.conf -daemon
     done
     sleep 3
 fi
@@ -68,12 +68,12 @@ echo
 echo -e "Waiting 10 seconds for all daemons to start up..."
 echo
 sleep 10
-echo -e "${blu}Update complete. Atheneum now updated to version below.${yel}"
-$workDir/atheneumd -datadir=/root/.Atheneum$i -conf=/root/.Atheneum$i/atheneum.conf --version | head -n 1
-echo -e "${blu}Version : ${grn}" $($workDir/atheneum-cli -datadir=/root/.Atheneum -conf=/root/.Atheneum/atheneum.conf getinfo | grep '"version":' | tr -d '"version:, ')
-echo -e "${blu}Protocol: ${grn}" $($workDir/atheneum-cli -datadir=/root/.Atheneum -conf=/root/.Atheneum/atheneum.conf getinfo | grep '"protocolversion":' | tr -d '"protocolversion:, ')
+echo -e "${blu}Update complete. Alqo now updated to version below.${yel}"
+$workDir/alqod -datadir=/root/.alqo$i -conf=/root/.alqo$i/alqo.conf --version | head -n 1
+echo -e "${blu}Version : ${grn}" $($workDir/alqo-cli -datadir=/root/.alqo -conf=/root/.alqo/alqo.conf getinfo | grep '"version":' | tr -d '"version:, ')
+echo -e "${blu}Protocol: ${grn}" $($workDir/alqo-cli -datadir=/root/.alqo -conf=/root/.alqo/alqo.conf getinfo | grep '"protocolversion":' | tr -d '"protocolversion:, ')
 echo
 echo -e "${red} !!! IMPORTANT !!! ${grn}"
 echo -e "Don't forget to update your QT wallet with the latest executables. \nYou can find them at the official repo at:"
-echo -e "${blu}https://github.com/AtheneumChain/Atheneum/releases ${clr}"
+echo -e "${blu}https://github.com/AtheneumChain/Atheneum/releases ${clr}"  #need static installer link
 echo
