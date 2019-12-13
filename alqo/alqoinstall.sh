e7d807561696ce06a1c7da8de2c7f24632f4497f
captaingeeky <35855107+captaingeeky@users.noreply.github.com>
2019-12-13 14:33:19 -0500

    Update alqoinstall.sh

--- alqo/alqoinstall.sh
+++ alqo/alqoinstall.sh
@@ -162,7 +162,8 @@ function install_prerequisites()
     echo
     #addid this for libdbcxx
     echo -ne "${GREEN} >Progress: ${BLUE}[###-----------]\r"
-    sudo apt install -y jq software-properties-common curl > /dev/null 2>&1
+    sudo apt-get install build-essential software-properties-common -y
+
     if [ $? -ne 0 ]; then
        echo 
        echo -e "${RED}Install of ${YELLOW}software-properties-common ${RED}and {$YELLOW}curl ${RED}failed! ${NC}"
@@ -170,12 +171,7 @@ function install_prerequisites()
     fi
     sudo apt update > /dev/null 2>&1
     echo -ne "${GREEN} >Progress: ${BLUE}[#####---------]\r"
-    sudo apt install -y pwgen build-essential libssl-dev libboost-all-dev libqrencode-dev libminiupnpc-dev libevent-2.0-5 > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
-       echo 
-       echo -e "${RED}Install of ${YELLOW}libmini, build-essential ${RED}and {$YELLOW}libevent ${RED}failed! ${NC}"
-       exit 1;
-    fi
+    
     echo -ne "${GREEN} >Progress: ${BLUE}[#######-------]\r"
     sudo add-apt-repository -y ppa:bitcoin/bitcoin > /dev/null 2>&1
     if [ $? -ne 0 ]; then
@@ -185,7 +181,7 @@ function install_prerequisites()
     fi
     sudo apt update > /dev/null 2>&1
     echo -ne "${GREEN} >Progress: ${BLUE}[##########----]\r"
-    sudo apt install -y jq libdb5.3-dev libdb5.3++-dev > /dev/null 2>&1
+    sudo apt-get install libtool bsdmainutils autotools-dev autoconf pkg-config automake python3 libssl-dev libgmp-dev libevent-dev libboost-all-dev libdb4.8-dev libdb4.8++-dev libzmq3-dev libminiupnpc-dev -y
     if [ $? -ne 0 ]; then
        echo 
        echo -e "${RED}Install of ${YELLOW}libdb5.3 libraries ${RED}failed! ${NC}"
@@ -194,12 +190,6 @@ function install_prerequisites()
     echo -ne "${GREEN} >Progress: ${BLUE}[############--]${NC}\r"
     #end libdbcxx section
   
-    sudo apt install -y jq libzmq5 libboost-system1.58.0 libboost-filesystem1.58.0 libboost-program-options1.58.0 libboost-thread1.58.0 libboost-chrono1.58.0 libminiupnpc10 libevent-pthreads-2.0-5 unzip > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
-       echo 
-       echo -e "${RED}Install of ${YELLOW}libboost ${RED}and {$YELLOW}extra files ${RED}failed! ${NC}"
-       exit 1;
-    fi
     echo -ne "${GREEN} >Progress: ${BLUE}[##############]${NC}"
     echo
   fi
