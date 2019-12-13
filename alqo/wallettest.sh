7ae5fcceba284cf7b492d1031425ae911a7f56f6
captaingeeky <35855107+captaingeeky@users.noreply.github.com>
2019-12-13 12:42:34 -0500

    Create wallettest.sh

---
+++ alqo/wallettest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install build-essential software-properties-common -y
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
+git clone git@github.com:ALQO-Universe/ALQO.git
+cd ALQO
+sudo apt-get install libtool bsdmainutils autotools-dev autoconf pkg-config automake python3 libssl-dev libgmp-dev libevent-dev libboost-all-dev libdb4.8-dev libdb4.8++-dev libzmq3-dev libminiupnpc-dev -y
+./autogen.sh
+./configure --without-gui --disable-tests
+make
+mv src/alqod ~
+mv src/alqo-cli ~
+cd ~
+ls
