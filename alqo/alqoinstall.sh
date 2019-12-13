4b379cb1a07261d60db715b7a041ce22423fc6e0
captaingeeky <35855107+captaingeeky@users.noreply.github.com>
2019-12-13 14:40:51 -0500

    Update alqoinstall.sh

--- alqo/alqoinstall.sh
+++ alqo/alqoinstall.sh
@@ -204,6 +204,7 @@ function copy_binaries()
     
     git clone https://github.com/ALQO-Universe/ALQO.git
     sudo apt-get install libtool bsdmainutils autotools-dev autoconf pkg-config automake python3 libssl-dev libgmp-dev libevent-dev libboost-all-dev libdb4.8-dev libdb4.8++-dev libzmq3-dev libminiupnpc-dev -y
+    cd ALQO
     ./autogen.sh
     ./configure --without-gui --disable-tests
     make
