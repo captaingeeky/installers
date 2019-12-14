838d3246eca86c3f37529ce5d8297f1d15c343c2
captaingeeky <35855107+captaingeeky@users.noreply.github.com>
2019-12-14 17:13:55 -0500

    Update wallettest.sh

--- alqo/wallettest.sh
+++ alqo/wallettest.sh
@@ -1,15 +1,5 @@
 # cd ~ && wget https://raw.githubusercontent.com/captaingeeky/installers/master/alqo/wallettest.sh -O wallettest.sh && chmod +x wallettest.sh && ./wallettest.sh
 
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install build-essential software-properties-common -y
-sudo add-apt-repository ppa:bitcoin/bitcoin
-sudo apt-get update
-fallocate -l 3G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 git clone https://github.com/ALQO-Universe/ALQO.git
 cd ALQO
 sudo apt-get install libtool bsdmainutils autotools-dev autoconf pkg-config automake python3 libssl-dev libgmp-dev libevent-dev libboost-all-dev libdb4.8-dev libdb4.8++-dev libzmq3-dev libminiupnpc-dev -y
