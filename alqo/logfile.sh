036a0c0d2437efc50a621019e446edc5385a7bc5
captaingeeky <35855107+captaingeeky@users.noreply.github.com>
2020-01-11 13:43:08 -0500

    Update logfile.sh

--- alqo/logfile.sh
+++ alqo/logfile.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 # cd ~ && wget https://raw.githubusercontent.com/captaingeeky/installers/master/alqo/logfile.sh -O logfile.sh && chmod +x logfile.sh && mv ./logfile.sh /etc/cron.daily
 rm -rf ~/.alqocrypto/debug.log
